7d5bd855274201604c398301ad1173409c0cab24
Adriano A Costa <sdcomputadores@gmail.com>
2017-11-15 13:24:15 -0200

    removendo as repetições

--- src/Dezenas.java
+++ src/Dezenas.java
@@ -4,17 +4,25 @@ public class Dezenas {
 
 	/**
 	 * Gera um array de dezenas sem repetição
+	 * 
 	 * @return
 	 */
 	public static int[] gerarDezenas() {
 
+		Dezenas dez = new Dezenas();
+
 		int[] dezenas = new int[6];
 		Random random = new Random();
-		
+
 		int i = 0;
-		while(i < dezenas.length){
-			dezenas[i] = random.nextInt(10);
-			++i;
+		int numero = 0;
+		while (i < dezenas.length) {
+			numero = random.nextInt(10);
+			if (dez.checaArray(dezenas, numero) == false) {
+				dezenas[i] = numero;
+				++i;
+			}
+
 		}
 
 		return dezenas;
@@ -22,6 +30,7 @@ public class Dezenas {
 
 	/**
 	 * Converte um array em string
+	 * 
 	 * @param data
 	 * @return
 	 */
@@ -29,9 +38,18 @@ public class Dezenas {
 
 		String numeros = new String();
 		for (int i = 0; i < data.length; i++) {
-			numeros += ","+ data[i];
+			numeros += "," + data[i];
 		}
 		return numeros;
+	}
+
+	public static boolean checaArray(int[] data, int value) {
+		for (int i = 0; i < data.length; i++) {
+			if (data[i] == value) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 }
